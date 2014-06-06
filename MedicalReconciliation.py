52172c65ff629e7f52903ef4a3e7111e800ece59
fandt <favoriteplum@hotmail.com>
2014-06-06 15:42:37 -0700

    first commit

--- MedicalReconciliation.py
+++ MedicalReconciliation.py
@@ -2,7 +2,7 @@ def test_sums():
   import csv
   import Tkinter, tkFileDialog
   import os
-  file = "C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon"
+  file = "path/filetocreate"
   krecon = open(file,"w+")
   root = Tkinter.Tk()
   marker = 0
@@ -14,7 +14,7 @@ def test_sums():
   global found
   oddeven = 0
   missing = "blank"
-  currentfile = tkFileDialog.askopenfilename(parent=root,initialdir="C:/Users/rmorganstern/Documents/Medent Reconcile/",title='Choose the current deduction report')
+  currentfile = tkFileDialog.askopenfilename(parent=root,initialdir="C:originpath",title='Choose the current deduction report')
   deductionfile = csv.reader(open(currentfile))
   for drow in deductionfile:
     if marker > 0:
@@ -28,16 +28,16 @@ def test_sums():
         except:
           pass
         if marker%2 == 0 :
-          with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
+          with open("C:filethatwascreated", 'a') as krecon:
             krecon.write(str(invoicetotal)+"\t"+InvoiceType+"\n")
           #print
           out = str(deductSS)+"\t"+str(name)+"\t"+str(deductotal)+"\t"
-          with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
+          with open("C:filethatwascreated", 'a') as krecon:
             krecon.write(out)
           
           found = 0
           #get invioce amount from downloaded files
-          with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/DHMO Union.csv") as csvfile:
+          with open("C:invoice.csv") as csvfile:
             medent = csv.reader(csvfile)
             InvoiceType = "DHMO Union"
 
@@ -55,100 +55,6 @@ def test_sums():
               except:
                 continue 
 
-            if found == 0:
-              with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO 2788.csv") as csvfile:
-                medent1 = csv.reader(csvfile)
-                InvoiceType = "HMO 2788"
-
-  #def test_download(medent,deductotal): HMO 2788
-                for mrow in medent1:
-                  try:
-                    invoiceSS = mrow[4]
-                    if invoiceSS == deductSS:
-                      invoicetotal = float(mrow[9])
-                      if abs(invoicetotal - deductotal) >= 1:
-                        print name + " deduction amount= " + str(deductotal) + " HMO 2788 invoice amount= " + str(mrow[9])
-                        deductotal = 0
-                      found = 1
-                      break
-                  except:
-                    continue
-
-                if found == 0:
-                  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/DHMO.csv") as csvfile:
-                      medent2 = csv.reader(csvfile)
-                      InvoiceType = "DHMO"
-
-  #def test_download(medent,deductotal):  DHMO
-                      for mrow in medent2:
-                        try:
-                          invoiceSS = mrow[4]
-                          if invoiceSS == deductSS:
-                            invoicetotal = float(mrow[9])
-                            if abs(invoicetotal - deductotal) >= 1:
-                              print name + " deduction amount= " + str(deductotal) + " DHMO invoice amount= " + str(mrow[9])
-                              deductotal = 0
-                            found = 1
-                            break
-                        except:
-                          continue
-                      
-                      if found == 0:
-                        with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO 2788 Union.csv") as csvfile:
-                          medent3 = csv.reader(csvfile)
-                          InvoiceType = "HMO 2788 Union"
-
-  #def test_download(medent,deductotal):  HMO 2788 Union
-                          for mrow in medent3:
-                            try:
-                              invoiceSS = mrow[4]
-                              if invoiceSS == deductSS:
-                                invoicetotal = float(mrow[9])
-                                if abs(invoicetotal - deductotal) >= 1:
-                                  print name + " deduction amount= " + str(deductotal) + " HMO 2788 Union invoice amount= " + str(mrow[9])
-                                  deductotal = 0
-                                found = 1
-                                break
-                            except:
-                              continue
-
-                          if found == 0:                    
-                            with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO Union.csv") as csvfile:
-                              medent4 = csv.reader(csvfile)
-                              InvoiceType = "HMO Union"
-
-  #def test_download(medent,deductotal): HMO Union
-                              for mrow in medent4:
-                                try:
-                                  invoiceSS = mrow[4]
-                                  if invoiceSS == deductSS:
-                                    invoicetotal = float(mrow[9])
-                                    if abs(invoicetotal - deductotal) >= 1:
-                                      print name + " deduction amount= " + str(deductotal) + " HMO Union invoice amount= " + str(mrow[9])
-                                      deductotal = 0
-                                    found = 1
-                                    break
-                                except:
-                                  continue
-                        
-                              if found == 0:
-                                with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO.csv") as csvfile:
-                                  medent5 = csv.reader(csvfile)
-                                  InvoiceType = "HMO"
-
-  #def test_download(medent,deductotal):  HMO
-                                  for mrow in medent5:
-                                    try:
-                                      invoiceSS = mrow[4]
-                                      if invoiceSS == deductSS:
-                                        invoicetotal = float(mrow[9])
-                                        if abs(invoicetotal - deductotal) >= 1:
-                                          print name + " deduction amount= " + str(deductotal) + " HMO invoice amount= " + str(mrow[9])
-                                          deductotal = 0
-                                        found = 1
-                                        break
-                                    except:
-                                      continue 
           deductotal = 0
           if found == 0:
             print name + " NOT FOUND IN INVOICES."
@@ -158,13 +64,13 @@ def test_sums():
 
 
 #BACK
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
+  with open("C:filethatwascreated", 'a') as krecon:
       krecon.write("\nNOT FOUND IN DEDUCTIONS")
       SumInvoice = 0
       
 #deduction file 0 DHMO UNION
   deductionfile = csv.reader(open(currentfile))
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/DHMO Union.csv") as csvfile:
+  with open("C:invoice.csv") as csvfile:
     invoicefile = csv.reader(csvfile)
     file = "DHMO Union"
     for irow in invoicefile:
@@ -184,118 +90,9 @@ def test_sums():
         with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
             krecon.write("\n"+str(irow[4])+"\t" + str(irow[2])+"\t\t" +str(irow[9]))
         deductionfile = csv.reader(open(currentfile))
-#deduction file 1  HMO 2788
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO 2788.csv") as csvfile:
-    deductionfile = csv.reader(open(currentfile))
-    invoicefile1 = csv.reader(csvfile)
-    file = "HMO 2788"
-    for irow in invoicefile1:
-      deductionfile = csv.reader(open(currentfile))
-      found = 0
-      try:
-          invoiceSS = irow[4]
-      except:
-          continue
-      for drow in deductionfile:
-        deductSS = drow[6]
-        if invoiceSS == deductSS:
-          found = 1
-          break
-      if found == 0 and irow[2] != "" and irow[9] != "" and irow[9] != "Total dues":
-        print irow[2] + " Not found in deductions " + file + "\t" + str(irow[9])
-        with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
-            krecon.write("\n"+str(irow[4])+"\t" + str(irow[2])+"\t\t" +str(irow[9]))
-        deductionfile = csv.reader(open(currentfile))
-#deduction file 2 DHMO
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/DHMO.csv") as csvfile:
-    deductionfile = csv.reader(open(currentfile))
-    invoicefile2 = csv.reader(csvfile)
-    file = "DHMO"
-    for irow in invoicefile2:
-      deductionfile = csv.reader(open(currentfile))
-      found = 0
-      try:
-          invoiceSS = irow[4]
-      except:
-          continue
-      for drow in deductionfile:
-        deductSS = drow[6]
-        if invoiceSS == deductSS:
-          found = 1
-          break
-      if found == 0 and irow[2] != "" and irow[9] != "" and irow[9] != "Total dues":
-        print irow[2] + " Not found in deductions " + file  + "\t" + str(irow[9])
-        with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
-            krecon.write("\n"+str(irow[4])+"\t" + str(irow[2])+"\t\t" +str(irow[9]))
-        deductionfile = csv.reader(open(currentfile))
-#deduction file 3 HMO 2788 UNION
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO 2788 Union.csv") as csvfile:
-    deductionfile = csv.reader(open(currentfile))
-    invoicefile3 = csv.reader(csvfile)
-    file = "2788"
-    for irow in invoicefile3:
-      deductionfile = csv.reader(open(currentfile))
-      found = 0
-      try:
-          invoiceSS = irow[4]
-      except:
-          continue
-      for drow in deductionfile:
-        deductSS = drow[6]
-        if invoiceSS == deductSS:
-          found = 1
-          break
-      if found == 0 and irow[2] != "" and irow[9] != "" and irow[9] != "Total dues":
-        print irow[2] + " Not found in deductions " + file + "\t" + str(irow[9])
-        with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
-            krecon.write("\n"+str(irow[4])+"\t" + str(irow[2])+"\t\t" +str(irow[9]))
-        deductionfile = csv.reader(open(currentfile))
-#deduction file 4  HMO UNION
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO Union.csv") as csvfile:
-    deductionfile = csv.reader(open(currentfile))
-    invoicefile4 = csv.reader(csvfile)
-    file = "HMO Union"
-    for irow in invoicefile4:
-      deductionfile = csv.reader(open(currentfile))
-      found = 0
-      try:
-          invoiceSS = irow[4]
-      except:
-          continue
-      for drow in deductionfile:
-        deductSS = drow[6]
-        if invoiceSS == deductSS:
-          found = 1
-          break
-      if found == 0 and irow[2] != "" and irow[9] != "" and irow[9] != "Total dues":
-        print irow[2] + " Not found in deductions " + file + "\t" + str(irow[9])
-        with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
-            krecon.write("\n"+str(irow[4])+"\t" + str(irow[2])+"\t\t" +str(irow[9]))
-        deductionfile = csv.reader(open(currentfile))
-#deduction file 5  HMO
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Downloads/HMO.csv") as csvfile:
-    deductionfile = csv.reader(open(currentfile))
-    invoicefile5 = csv.reader(csvfile)
-    file = "HMO"
-    for irow in invoicefile5:
-      deductionfile = csv.reader(open(currentfile))  
-      found = 0
-      try:
-          invoiceSS = irow[4]
-      except:
-          continue
-      for drow in deductionfile:
-        deductSS = drow[6]
-        if invoiceSS == deductSS:
-          found = 1
-          break
-      if found == 0 and irow[2] != "" and irow[9] != "" and irow[9] != "Total dues":
-        print irow[2] + " Not found in deductions " + file + "\t" + str(irow[9])
-        with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
-            krecon.write("\n"+str(irow[4])+"\t" + str(irow[2])+"\t\t" +str(irow[9]))
-        deductionfile = csv.reader(open(currentfile))
+
   
-  with open("C:/Users/rmorganstern/Documents/Medent Reconcile/Kaiser/krecon", 'a') as krecon:
+  with open("C:filethatwascreated", 'a') as krecon:
     krecon.write("\n\n" +"Total\t\t"+ str(SumInvoice))      
     
   raw_input("press enter to exit")
